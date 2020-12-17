2dbeef657445aaa560d2721f536f86f180c6247a
tusharkhandejod <tusharkhandejod@gmail.com>
2020-12-17 19:47:43 +0530

    UC6 case = code for storing employee's daily wage in an array is reached is added

--- EmpWage.js
+++ EmpWage.js
@@ -1,31 +1,46 @@
-//UC5-->wages for month assuming 20 days and 100
+//UC6 --> Storing inside an Array
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
+
 const FULL_TIME_HOURS = 8;
 const PART_TIME_HOURS = 4;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
-const MAX_HOURS_IN_MONTH = 100
+const MAX_HRS_IN_MONTH = 100;
+
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
 
 function getWorkingHours(empCheck) {
-
     switch (empCheck) {
         case IS_PART_TIME:
+            console.log("Employee is Present for Part Time");
             return PART_TIME_HOURS;
+
         case IS_FULL_TIME:
+            console.log("Employee is Present for Full Time");
             return FULL_TIME_HOURS;
+
         default:
+            console.log("Employee is Absent");
             return 0;
     }
 }
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
 
-while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-    totalWorkingDays++;
-    let empCheck = Math.floor(Math.round() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+function calcDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
 }
 
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("Total Days: " + totalWorkingDays + " Total Hrs:" + totalEmpHrs + "   Emp Wage: " + empWage);
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
+}
+let empWage = calcDailyWage(totalEmpHrs);
+console.log("Total Working Days: " + totalWorkingDays + "  Total Working Hrs: " + totalEmpHrs + "  Emp Monthly Wage: " + empWage);
+
