ccf6ed0dd9e8c7922a36b0dfaa8132c1e5b2154b
tusharkhandejod <tusharkhandejod@gmail.com>
2020-12-17 19:39:01 +0530

    UC4 case = code for calculating employee's monthly salary is added

--- EmpWage.js
+++ EmpWage.js
@@ -1,13 +1,16 @@
-//UC3 -->  write a function for daily working hours 
+//UC4 -->wages for month assuming 20 days
 const IS_ABSENT = 0;
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
-const WAGE_PER_HR = 20;
+const WAGE_PER_HOUR = 20;
 const FULL_TIME_HOURS = 8;
 const PART_TIME_HOURS = 4;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck) {
+
     switch (empCheck) {
+
         case IS_FULL_TIME:
             return FULL_TIME_HOURS;
         case IS_PART_TIME:
@@ -19,9 +22,12 @@ function getWorkingHours(empCheck) {
             console.log("This is default case.");;
     }
 }
+
 let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHours(empCheck);
-let dailyEmpWage = empHrs * WAGE_PER_HR;
-console.log("Daily Emp Wage : " + dailyEmpWage);
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Total Hours: " + empHrs + "   Emp Wage: " + empWage);
 
